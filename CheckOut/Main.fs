20f0d8bbfe6759dd2995c401a1a04c44df63a3a6
Hackle Wayne <hacklew@hotmail.com>
2016-11-22 22:17:35 +1300

    Can calc combo but incorrectly

--- CheckOut/Main.fs
+++ CheckOut/Main.fs
@@ -4,35 +4,40 @@
 type Item = Item of char
 type Price = Price of int
 type Quantity = Quantity of int
-type Pricing = { Item: Item; Quantity: Quantity; Price: Price }
+type Pricing = { Items: (Item * Quantity) list; Price: Price }
 type PriceState = { Items: (Item * Quantity) list; Total: Price }
 
 let pricings = [
-    { Item = Item 'A'; Quantity = Quantity 3; Price = Price 130 }
-    { Item = Item 'A'; Quantity = Quantity 1; Price = Price 50 }
+    { Items = [ (Item 'A', Quantity 3) ]; Price = Price 130 }
+    { Items = [ (Item 'A', Quantity 1); (Item 'B', Quantity 1) ]; Price = Price 70 }
+    { Items = [ (Item 'A', Quantity 1) ]; Price = Price 50 }
+    { Items = [ (Item 'B', Quantity 1) ]; Price = Price 30 }
 ]
 
-let takeItems code qty (items: (Item * Quantity) list) =
-    let found = items |> List.tryFind (fun (Item it, Quantity q) -> it = code && q >= qty)
-    match found with
-    | None -> None
-    | Some foundItem -> 
-        let (Item it, Quantity q) = foundItem
-        let rest = items |> List.map (fun it' -> 
-                                        if it' = foundItem 
-                                        then (Item it, Quantity (q - qty)) 
-                                        else it')
-        Some rest
+let tryApply (pricing: Pricing) (items: (Item * Quantity) list) =
+    let take' (Item item, Quantity qty) =
+        let itemInPricing = pricing.Items |> List.tryFind (fun (Item it, _) -> it = item)
+        match itemInPricing with
+        | None -> None
+        | Some (_, Quantity qw) -> 
+            let q' = qty - qw
+            if q' < 0
+                then None
+                else Some (Item item, Quantity q')
+
+    let attempt = items |> List.map take'
+    if attempt |> List.exists Option.isNone
+        then None
+        else attempt |> List.map (fun v -> v.Value) |> Some
 
 let rec calcOne pricing (accu: PriceState) =
-    let { Item = Item code; Quantity = Quantity qty; Price = Price p } = pricing
+    let { Price = Price price } = pricing
     let { Items = items; Total = Price total } = accu
-    let rest = items |> takeItems code qty 
+    let rest = items |> tryApply pricing
 
     match rest with
     | None -> accu
-    | Some r -> calcOne pricing { Items = r; Total = Price (total + p) }
-    
+    | Some r -> calcOne pricing { Items = r; Total = Price (total + price) }    
 
 let calc itemCodes =
     let items = 
--- CheckOut/MainTests.fs
+++ CheckOut/MainTests.fs
@@ -12,3 +12,16 @@ open Xunit
 let ``Price for only 'A's`` (cnt, expectedPrice) =    
     let (Price price) = 'A' |> List.replicate cnt |> calc
     Assert.Equal(price, expectedPrice)
+
+[<Theory>]
+[<InlineData(1, 0, 50)>]
+[<InlineData(0, 1, 30)>]
+[<InlineData(1, 1, 70)>]
+[<InlineData(2, 1, 120)>]
+[<InlineData(1, 2, 100)>]
+let ``Price for combo of 'A' and 'B'`` (cntA, cntB, expectedPrice) =
+    let a's = 'A' |> List.replicate cntA
+    let b's = 'B' |> List.replicate cntB
+
+    let (Price price) = a's @ b's |> calc
+    Assert.Equal(price, expectedPrice)
