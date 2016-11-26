95e628dd7b563fd2306057e2885b22d6e256a25c
Hackle Wayne <hacklew@hotmail.com>
2016-11-26 13:22:19 +1300

    Have <pc> for pieace

--- CheckOut/Main.fs
+++ CheckOut/Main.fs
@@ -4,27 +4,27 @@ type Item = Item of char
 type Price = Price of int with
     static member (+) ((Price p1), (Price p2)) =  Price (p1 + p2)
 
-type Quantity = Quantity of int with
-    static member (-) ((Quantity qty1), (Quantity qty2)) = Quantity (qty1 - qty2)
+[<Measure>]
+type pc
 
-type SomeOfPricing = { Items: (Item * Quantity) list; Price: Price }
-type AnyOfPricing = { ChooseFrom: Item list; Quantity: Quantity; Price: Price }
+type SomeOfPricing = { Items: (Item * int<pc>) list; Price: Price }
+type AnyOfPricing = { ChooseFrom: Item list; Quantity: int<pc>; Price: Price }
 type Pricing = SomeOf of SomeOfPricing | AnyOf of AnyOfPricing
-type PriceState = { Items: (Item * Quantity) list; Total: Price }
+type PriceState = { Items: (Item * int<pc>) list; Total: Price }
 
 type List<'a> with
     static member replaceAt index newItem (lst: (int * 'a) list) =
         lst |> List.map (fun (i, v) -> if i = index then (i, newItem) else (i, v))
 
 let pricings: Pricing list = [
-    SomeOf { Items = [ (Item 'A', Quantity 3) ]; Price = Price 130 }
-    SomeOf { Items = [ (Item 'A', Quantity 1); (Item 'B', Quantity 1) ]; Price = Price 70 }
-    SomeOf { Items = [ (Item 'A', Quantity 1) ]; Price = Price 50 }
-    SomeOf { Items = [ (Item 'B', Quantity 1) ]; Price = Price 30 }
-    AnyOf { ChooseFrom = [ Item 'C'; Item 'D'; Item 'E' ]; Quantity = Quantity 2; Price = Price 10 }
-    SomeOf { Items = [ (Item 'C', Quantity 1) ]; Price = Price 5 }
-    SomeOf { Items = [ (Item 'D', Quantity 1) ]; Price = Price 6 }
-    SomeOf { Items = [ (Item 'E', Quantity 1) ]; Price = Price 7 }
+    SomeOf { Items = [ (Item 'A', 3<pc>) ]; Price = Price 130 }
+    SomeOf { Items = [ (Item 'A', 1<pc>); (Item 'B', 1<pc>) ]; Price = Price 70 }
+    SomeOf { Items = [ (Item 'A', 1<pc>) ]; Price = Price 50 }
+    SomeOf { Items = [ (Item 'B', 1<pc>) ]; Price = Price 30 }
+    AnyOf { ChooseFrom = [ Item 'C'; Item 'D'; Item 'E' ]; Quantity = 2<pc>; Price = Price 10 }
+    SomeOf { Items = [ (Item 'C', 1<pc>) ]; Price = Price 5 }
+    SomeOf { Items = [ (Item 'D', 1<pc>) ]; Price = Price 6 }
+    SomeOf { Items = [ (Item 'E', 1<pc>) ]; Price = Price 7 }
 ]
 
 let rec applySomeOfPricingOnce (pricing: SomeOfPricing) (priceState: PriceState) = 
@@ -50,21 +50,21 @@ let rec applySomeOfPricingOnce (pricing: SomeOfPricing) (priceState: PriceState)
             applySomeOfPricingOnce pricing { Items = rest; Total = total + pricing.Price }
 
 let rec applyAnyOfPricingOnce (pricing: AnyOfPricing) (priceState: PriceState) =
-    let { ChooseFrom = itemsToChooseFrom; Quantity = Quantity totalForPricing; Price = price } = pricing
+    let { ChooseFrom = itemsToChooseFrom; Quantity = totalForPricing; Price = price } = pricing
     let { Items = itemsToBuy; Total = total } = priceState
 
-    let cntAvailableToTake (item, Quantity qty) =
+    let cntAvailableToTake (item, qty) =
        if List.exists (fun it -> it = item) itemsToChooseFrom
         then qty
-        else 0
+        else 0<pc>
 
-    let rec takeTill' (cntTaken, itemsLeft) (index, (item, Quantity qty)) =
+    let rec takeTill' (cntTaken, itemsLeft) (index, (item, qty)) =
         let stillNeeded = totalForPricing - cntTaken
-        if stillNeeded = 0 || cntAvailableToTake (item, Quantity qty) = 0
+        if stillNeeded = 0<pc> || cntAvailableToTake (item, qty) = 0<pc>
             then (cntTaken, itemsLeft)
             else
                 let cntToTake = min qty stillNeeded
-                let afterTaking = item, Quantity (qty - cntToTake)
+                let afterTaking = item, qty - cntToTake
                 let stillLeft = itemsLeft |> List.replaceAt index afterTaking
                 cntTaken + cntToTake, stillLeft
 
@@ -73,7 +73,7 @@ let rec applyAnyOfPricingOnce (pricing: AnyOfPricing) (priceState: PriceState) =
         then priceState
         else
             let indexed = itemsToBuy |> List.indexed
-            let rest = List.fold takeTill' (0, indexed) indexed |> snd |> List.map snd
+            let rest = List.fold takeTill' (0<pc>, indexed) indexed |> snd |> List.map snd
             applyAnyOfPricingOnce pricing { Items = rest; Total = total + price }
 
 let applyPricingOnce (pricing: Pricing) (priceState: PriceState) =
@@ -86,7 +86,7 @@ let calc itemCodes =
         itemCodes
         |> List.map Item
         |> List.groupBy id
-        |> List.map (fun (it, ls) -> (it, Quantity (ls |> List.length)))
+        |> List.map (fun (it, ls) -> it, (List.length ls) * 1<pc>)
     let initialState = { Items = items; Total = Price 0 }
 
     let finalState =
